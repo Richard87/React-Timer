8293e838066b99ae90ebe88045026fc44987d6d4
Richard Hagen <richard.hagen@gmail.com>
2019-03-25 15:10:11 +0100

    better font

--- src/Components/StretchedText.js
+++ src/Components/StretchedText.js
@@ -13,8 +13,8 @@ const StyledDiv = styled.div`
 
 const StretchedText = ({color, children, ...props}) => {
     return <StyledDiv {...props} color={color}>
-        <svg viewBox="0 0 70 16">
-            <text x="35" y="15">
+        <svg viewBox="0 0 90 16">
+            <text x="45" y="15">
                 <tspan textAnchor="middle">{children}</tspan>
             </text>
         </svg>
